c2c4300812bb928e108990d78f8f5c954add00ec
pjo901018 <pjo901018@korea.ac.kr>
2017-01-14 12:51:34 +0900

    initial commit

---
+++ src/main/java/study/khs/YoutubeSynchronizerApplication.java
@@ -0,0 +1,12 @@
+package study.khs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeSynchronizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeSynchronizerApplication.class, args);
+	}
+}
