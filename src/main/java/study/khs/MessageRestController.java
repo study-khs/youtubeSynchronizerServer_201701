a179b811f31cc84026c64a24ecdcfc4fea5df414
scoll <scoll@192.168.1.102>
2017-01-14 18:01:06 +0900

    @PathVariable 변수 맞춰줘야함

--- src/main/java/study/khs/MessageRestController.java
+++ src/main/java/study/khs/MessageRestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/message/{path_message}")
+@RequestMapping("/message/{pathMessage}")
 public class MessageRestController {
 	//private final String message;
 
