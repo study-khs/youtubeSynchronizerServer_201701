6c2b1612c62fd2715132cef4e86f5ac04b337348
scoll <scoll@DESKTOP-AI17LJ7.local>
2017-01-28 19:20:43 +0900

    수정

--- src/main/java/study/khs/config/swagger/SwaggerConfig.java
+++ src/main/java/study/khs/config/swagger/SwaggerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,13 +13,20 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-	
+
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("study.khs.api"))
 				.paths(PathSelectors.ant("/api/**"))
-				.build();
+				.build()
+				.apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		ApiInfo apiInfo = new ApiInfo("API SPEC 확인용 페이지", "스터디를 위한 테스트 API SPEC 확인용 페이지입니다", "", "Terms of service",
+				"", "", "");
+		return apiInfo;
 	}
 }
