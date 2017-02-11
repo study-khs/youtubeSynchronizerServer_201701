68125172e5942a2ffe80590a948ba8151bb0e8aa
scoll <scoll@10.16.16.165>
2017-02-11 11:45:33 +0900

    인코딩 수정

--- src/main/java/study/khs/ServletInitializer.java
+++ src/main/java/study/khs/ServletInitializer.java
@@ -1,7 +1,13 @@
 package study.khs;
 
+import java.nio.charset.Charset;
+
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
@@ -10,4 +16,16 @@ public class ServletInitializer extends SpringBootServletInitializer {
 		return application.sources(YoutubeSynchronizerApplication.class);
 	}
 
+	@Bean
+	public HttpMessageConverter<String> responseBodyConverter() {
+		return new StringHttpMessageConverter(Charset.forName("UTF-8"));
+	}
+
+	@Bean
+	public CharacterEncodingFilter characterEncodingFilter() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		return characterEncodingFilter;
+	}
 }
