6c5cf0821fc104e9b8e7bc82f914891ff6baddd9
scoll <scoll@10.16.16.165>
2017-02-11 11:50:03 +0900

    인코딩 수정

--- src/main/java/study/khs/YoutubeSynchronizerApplication.java
+++ src/main/java/study/khs/YoutubeSynchronizerApplication.java
@@ -1,31 +1,12 @@
 package study.khs;
 
-import java.nio.charset.Charset;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootApplication
 public class YoutubeSynchronizerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YoutubeSynchronizerApplication.class, args);
-	}
-
-	@Bean
-	public HttpMessageConverter<String> responseBodyConverter() {
-		return new StringHttpMessageConverter(Charset.forName("UTF-8"));
-	}
-
-	@Bean
-	public CharacterEncodingFilter characterEncodingFilter() {
-		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
-		characterEncodingFilter.setEncoding("UTF-8");
-		characterEncodingFilter.setForceEncoding(true);
-		return characterEncodingFilter;
 	}
 }
